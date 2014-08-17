18d1412a7a8405084cb07b5d98d4fb1f6415f103
Damian <damian.strony@gmail.com>
2014-08-17 15:28:53 +0200

    add firstDayPosition and monthDays functions to JSON Event Calendar

--- js/dsJSONEventCalendar.js
+++ js/dsJSONEventCalendar.js
@@ -7,12 +7,39 @@
 
 
 (function( $ ) {
-    $.fn.JSONEventCalendar = function(options) {
-        console.log("JSONEventCalendar");
+    $.fn.JSONEventCalendar = function(events,options) {
 
         var settings = $.extend($.fn.JSONEventCalendar.settings, options );
 
-        function refreshDayHeight(element) {
+        return this.each( function() {
+            var that = $(this);
+//            var m = moment();
+
+//            m.locale(settings.lang);
+//            m.format(settings.format);
+
+            _refreshDayHeight(that);
+//            console.log(_monthDays(m.get('year'), m.get('month')));
+
+
+            that.on('click','.ds-day',function(){
+//                console.log(settings.monthNames);
+//                var now = moment();
+//                console.warn(m.format(settings.format));
+            })
+
+
+        });
+
+
+
+        /**
+         * Refresh height of element to equal size
+         *
+         * @param element
+         * @private
+         */
+        function _refreshDayHeight(element) {
             var maxHeight = 0;
             element.find('.ds-events').each(function(){
                 var that = $(this);
@@ -24,23 +51,49 @@
             });
         }
 
-        return this.each( function() {
-            var that = $(this);
-            var m = moment();
+        /**
+         * Return number of days in ponth
+         *
+         * @param year
+         * @param month
+         * @returns {number}
+         * @private
+         */
+        function _monthDays(year,month) {
+            month++;
+            var d = new Date(year,month, 0);
+            return d.getDate();
+        }
 
-            m.locale(settings.lang);
-            m.format(settings.format);
+        /**
+         * Return number of day in week
+         * Default sunday is first
+         *
+         * @param year
+         * @param month
+         * @param offset
+         * @returns {*}
+         * @private
+         */
+        function _firstDayPosition(year,month,offset) {
+            month++;
+            var mom = moment();
+            mom.set('year',year);
+            mom.set('month',month);
+            return mom.day()+offset;
+        }
 
-            refreshDayHeight(that);
+
+        function _drawCalendar() {
+            var calendar = "";
 
 
-            that.on('click','.ds-day',function(){
-                console.log(settings.monthNames);
-//                var now = moment();
-                console.warn(m.format(settings.format));
-            })
-        });
+            return "";
+        }
 
+        function _drawList(){
+
+        }
     }
 }( jQuery ));
 
@@ -50,6 +103,7 @@ $.fn.JSONEventCalendar.settings = {
     lang: "pl",
 //    format: "DD-MM-YYYY",
     format: "DD MMMM YYYY",
+    startFrom: "monday",
     monthNames: [ "January", "February", "March", "April", "May", "June","July", "August", "September", "October", "November", "December" ],
     dayNames: [ 'Sunday', 'Monday', 'Tuesday', 'Wednesday','Thursday', 'Friday', 'Saturday' ],
     dayNamesShort: [ 'Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat' ]
